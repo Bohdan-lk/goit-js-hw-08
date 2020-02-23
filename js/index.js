90a25b101c0a1be6c13d0b33f199edc46d2a8b8c
Bogdan <bogdan.lysenko.k@gmail.com>
2020-02-23 20:09:10 +0200

    v-41 fix

--- js/index.js
+++ js/index.js
@@ -1,84 +1,61 @@
 'use strict';
 
-import galleryItems from './gallery-items.js';
+import  galleryItems from './gallery-items.js';
 
-const gallery = {
-    jsGallery: document.querySelector ('.js-gallery'),
-    lightbox: document.querySelector ('.js-lightbox'),
-    ghtboxImage: document.querySelector ('.lightbox__image'),
-    ghtboxContent: document.querySelector ('.lightbox__content'),
-    ghtboxButton: document.querySelector ('.lightbox__button'),
-    ghtboxOverlay: document.querySelector ('.lightbox__overlay'),
+const galleryList = document.querySelector(".js-gallery");
+const boxContent = document.querySelector(".lightbox__content");
+const boxImg = document.querySelector(".lightbox__image");
+const boxButton = document.querySelector(".lightbox");
+const boxBtnClose = document.querySelector(".lightbox__button");
+
+const addList = ({ preview, original, description }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link"
+    href="${original}">
+    <img class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}">
+  </a>
+</li>`;
 };
 
-let nextCount = 0;
+const addRenderList = link =>
+    link.reduce((acc, el) => acc + addList(el), "");
 
-const addList = galleryItems
-    .map ((elem, indx) => `<li class = gallery__item> 
-<a class = gallery__link href=${elem.original}>
-<img class = gallery__image 
-src=${elem.preview} 
-data-source=${elem.original} 
-data-alt=${elem.description} data-pos=${indx} />
- </a>
- </li>`).join (" ");
+galleryList.insertAdjacentHTML("beforeend", addRenderList(galleryItems));
 
-gallery.jsGallery.insertAdjacentHTML ("beforeend", addList);
+galleryList.addEventListener("click", modalOpen);
 
-const openLightbox = (event) => {
+function modalOpen(e) {
+    e.preventDefault();
+    boxImg.setAttribute("src", e.target.dataset.source);
+    boxImg.setAttribute("alt", e.target.alt);
+    boxButton.classList.add("is-open");
+}
 
-    const galleryLink = Array.from (document.querySelectorAll ('.gallery__link'));
-    galleryLink.forEach (item => {
-        if (
-            event.target.getAttribute ('data-source') === item.getAttribute ('href')
-        ) {
-            item.removeAttribute ('href');
-        }
+boxBtnClose.addEventListener("click", modalClose);
 
-    });
-
-    gallery.lightbox.classList.add ('is-open');
-    gallery.ghtboxImage.setAttribute ('src', event.target.getAttribute ('data-source'));
-    gallery.ghtboxImage.setAttribute ('alt', event.target.getAttribute ('alt'));
-
-};
-
-function closeLightbox(event) {
-    gallery.lightbox.classList.remove ('is-open');
-    gallery.ghtboxImage.setAttribute ('src', '');
-    gallery.ghtboxImage.setAttribute ('alt', '');
-};
-
-function closeEsc(event) {
-    if (event.keyCode === 27) closeLightbox ();
-};
-
-function closeOverlay(event) {
-    if (event.target === gallery.ghtboxContent) closeLightbox (event);
-};
-
-
-const handleClickBtn = evt => {
-
-    if (evt.target.className === "next") {
-        nextCount++;
-        console.log ("click next");
-        console.log (nextCount);
-        if (nextCount <= galleryItems.length) {
-            gallery.ghtboxImage.setAttribute ('src', `${galleryItems[nextCount].original}`)
-        } else {
-            nextCount = 0;
-        }
-    } else if (evt.target.className === "prev") {
-        nextCount--;
-        console.log ("click prev");
-        console.log (nextCount);
-        gallery.ghtboxImage.setAttribute ('src', `${galleryItems[nextCount].original}`);
+boxContent.addEventListener("click", e => {
+    if (e.currentTarget === e.target){
+        modalClose()
     }
-};
+});
 
-gallery.jsGallery.addEventListener ('click', openLightbox);
-gallery.ghtboxButton.addEventListener ('click', closeLightbox);
-window.addEventListener ('keydown', closeEsc);
-gallery.ghtboxContent.addEventListener ('click', closeOverlay);
-gallery.ghtboxButton.addEventListener ('click', handleClickBtn);
+document.addEventListener("keydown", e => {
+    if (e.code === "Escape"){
+        modalClose()
+    }
+});
+
+function modalClose(e) {
+    boxImg.setAttribute("src", "");
+    boxImg.setAttribute("alt", "");
+    boxButton.classList.remove("is-open");
+}
+function handleKeyPress(occasion) {
+    if (occasion.code !== "Escape") {
+        return;
+    }
+    closeModal();
+}
